44c2222072f3704dce0206f18d12b8444209ab6c
Frank <reppepf@gmail.com>
2024-04-05 23:50:44 +0200

    fixed comparing to 0 issue (which determines type of solution

--- zad2/gauss.py
+++ zad2/gauss.py
@@ -6,10 +6,9 @@ EPS = 10e-15
 def solve(equation_system):
     unknowns_q = len(equation_system[0])
     equation_q = len(equation_system)
-    solution = []
-    for i in range(0, unknowns_q - 1):
-        solution.append(0.0)
-    for i in range(0, equation_q - 1):  # i -> numer równania, które odejmujemy od pozostałych
+    solution = [0.0 for _ in range(unknowns_q - 1)]
+
+    for i in range(equation_q - 1):  # i -> numer równania, które odejmujemy od pozostałych
         if abs(equation_system[i][i]) < EPS:  # jeżeli współczynnik < 10e-15 to wtedy python odczytuje to jako
             for k in range(i + 1, unknowns_q - 1):  # swap wierszy
                 if abs(equation_system[k][i]) > EPS:
@@ -23,14 +22,17 @@ def solve(equation_system):
             mul = -equation_system[j][i] / equation_system[i][i]
             for k in range(i, unknowns_q):  # k -> numer niewiadomej
                 equation_system[j][k] += mul * equation_system[i][k]
-    if equation_system[-1][-2] == 0 and equation_system[-1][-1] != 0:
+
+    if abs(equation_system[-1][-2]) < EPS < abs(equation_system[-1][-1]):  # sprawdzenie sprzeczności np 0 == 1
         return "sprzeczny"
-    for i in reversed(range(0, equation_q)):
+
+    for i in reversed(range(equation_q)):
+        if abs(equation_system[i][i]) < EPS:  # sprawdzenie czy 0 == 0 czyli dużo rozwiązań
+            return "nieoznaczony"
+
         b = equation_system[i][-1]
         for j in reversed(range(i + 1, equation_q)):
             b -= equation_system[i][j] * solution[j]
-        if abs(equation_system[i][i]) < EPS:
-            return "nieoznaczony"
         solution[i] = b / equation_system[i][i]
 
     return np.round(np.array(solution), 2)
--- zad2/main.py
+++ zad2/main.py
@@ -2,9 +2,7 @@ import file_handler as fh
 import gauss as g
 import os
 
-# equation_system = fh.read_equation_system("equations/" + "7equation_system.txt")
 directory = "equations/"
-
 
 for files in os.listdir(directory):
     print(files)
