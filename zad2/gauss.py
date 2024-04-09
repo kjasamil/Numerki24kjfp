51bb8c9f314eeca1c0d59e625cf8a51af4acd137
kjasamil <kamil200202@gmail.com>
2024-04-09 15:29:40 +0200

    New condition and changed when it is checked if the system has infinite solutions

--- zad2/gauss.py
+++ zad2/gauss.py
@@ -5,20 +5,22 @@ EPS = 10e-15
 
 def solve(equation_system):
     n = len(equation_system[0]) - 1
-    print(n)
     m = len(equation_system)
-    print(m)
     if n != m:
         return "Nieodpowiednia liczba niewiadomych i równań"
     solution = [0.0 for _ in range(n)]
     for i in range(n - 1):  # i -> numer równania, które odejmujemy od pozostałych
+        swap = False
         if abs(equation_system[i][i]) < EPS:  # jeżeli współczynnik < 10e-15 to wtedy python odczytuje to jako
             for k in range(i + 1, n):  # swap wierszy
                 if abs(equation_system[k][i]) > EPS:
                     temp = equation_system[i]
                     equation_system[i] = equation_system[k]
                     equation_system[k] = temp
+                    swap = True
                     break
+            if not swap:
+                return "Nie jest możliwe rozwiązanie tego układu równań."
         for j in range(i + 1, n):  # j -> numer równania, od którego odejmujemy równanie o numerze i
             # wyznaczamy mnożnik, przez który pomnożone zostanie równanie o numerze i oraz dodane do równania o nr j
             mul = -equation_system[j][i] / equation_system[i][i]
@@ -28,9 +30,10 @@ def solve(equation_system):
     if abs(equation_system[-1][-2]) < EPS < abs(equation_system[-1][-1]):  # sprawdzenie sprzeczności np 0 == 1
         return "sprzeczny"
 
+    if abs(equation_system[-1][-2]) < EPS and abs(equation_system[-1][-1]) < EPS:
+        return "nieoznaczony"
+
     for i in reversed(range(n)):
-        if abs(equation_system[i][i]) < EPS:  # sprawdzenie czy 0 == 0 czyli dużo rozwiązań
-            return "nieoznaczony"
         b = equation_system[i][-1]
         for j in reversed(range(i + 1, n)):
             b -= equation_system[i][j] * solution[j]
