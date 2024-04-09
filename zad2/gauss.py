8df28f7dba1f650adfbf0b91422bfcb670ca1c32
kjasamil <kamil200202@gmail.com>
2024-04-09 17:04:27 +0200

    Improved menu

--- zad2/gauss.py
+++ zad2/gauss.py
@@ -3,40 +3,40 @@ import numpy as np
 EPS = 10e-15
 
 
-def solve(equation_system):
-    n = len(equation_system[0]) - 1
-    m = len(equation_system)
+def solve(AB):
+    n = len(AB[0]) - 1
+    m = len(AB)
     if n != m:
         return "Nieodpowiednia liczba niewiadomych i równań"
     solution = [0.0 for _ in range(n)]
     for i in range(n - 1):  # i -> numer równania, które odejmujemy od pozostałych
         swap = False
-        if abs(equation_system[i][i]) < EPS:  # jeżeli współczynnik < 10e-15 to wtedy python odczytuje to jako
+        if abs(AB[i][i]) < EPS:  # jeżeli współczynnik < 10e-15 to wtedy python odczytuje to jako
             for k in range(i + 1, n):  # swap wierszy
-                if abs(equation_system[k][i]) > EPS:
-                    temp = equation_system[i]
-                    equation_system[i] = equation_system[k]
-                    equation_system[k] = temp
+                if abs(AB[k][i]) > EPS:
+                    temp = AB[i]
+                    AB[i] = AB[k]
+                    AB[k] = temp
                     swap = True
                     break
             if not swap:
                 return "Nie jest możliwe rozwiązanie tego układu równań."
         for j in range(i + 1, n):  # j -> numer równania, od którego odejmujemy równanie o numerze i
             # wyznaczamy mnożnik, przez który pomnożone zostanie równanie o numerze i oraz dodane do równania o nr j
-            mul = -equation_system[j][i] / equation_system[i][i]
+            mul = -AB[j][i] / AB[i][i]
             for k in range(i, n + 1):
-                equation_system[j][k] += mul * equation_system[i][k]
+                AB[j][k] += mul * AB[i][k]
 
-    if abs(equation_system[-1][-2]) < EPS < abs(equation_system[-1][-1]):  # sprawdzenie sprzeczności np 0 == 1
+    if abs(AB[-1][-2]) < EPS < abs(AB[-1][-1]):  # sprawdzenie sprzeczności np 0 == 1
         return "sprzeczny"
 
-    if abs(equation_system[-1][-2]) < EPS and abs(equation_system[-1][-1]) < EPS:
+    if abs(AB[-1][-2]) < EPS and abs(AB[-1][-1]) < EPS:
         return "nieoznaczony"
 
     for i in reversed(range(n)):
-        b = equation_system[i][-1]
+        b = AB[i][-1]
         for j in reversed(range(i + 1, n)):
-            b -= equation_system[i][j] * solution[j]
-        solution[i] = b / equation_system[i][i]
+            b -= AB[i][j] * solution[j]
+        solution[i] = b / AB[i][i]
 
     return np.round(np.array(solution), 2)
--- zad2/main.py
+++ zad2/main.py
@@ -2,11 +2,34 @@ import file_handler as fh
 import gauss as g
 import os
 
-directory = "equations/"
-
-for files in os.listdir(directory):
-    print(files)
-    equation_system = fh.read_equation_system(directory + str(files))
-    print(equation_system)
-    print(g.solve(equation_system))
-    print()
+while True:
+    print("1. Rozwiąż wszystkie układy równań z danego folderu")
+    print("2. Rozwiąż układ równań z pliku")
+    print("3. Wyjście")
+    case = input("Wybór: ")
+    if case == '1':
+        directory = input("Podaj ścieżkę do folderu: ")
+        if os.path.exists(directory):
+            for file in os.listdir(directory):
+                print(file)
+                AB = fh.read_equation_system(directory+str(file))
+                print("Układ równań:")
+                for row in AB:
+                    print(row)
+                print("Rozwiązanie: ", g.solve(AB))
+                print("")
+        else:
+            print("Nie ma takiego folderu!")
+    if case == '2':
+        filename = input("Podaj ścieżkę do pliku: ")
+        if os.path.exists(filename):
+            AB = fh.read_equation_system(filename)
+            print("Układ równań:")
+            for row in AB:
+                print(row)
+            print("Rozwiązanie: ", g.solve(AB))
+            print("")
+        else:
+            print("Nie ma takiego pliku!")
+    if case == '3':
+        break
