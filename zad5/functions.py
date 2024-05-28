6780f6701c38f925977df47d80975549597e8b14
Frank <reppepf@gmail.com>
2024-05-28 20:59:07 +0200

    functions

---
+++ zad5/functions.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def func1(x):
+    return 2*x+2
+
+
+def func2(x):
+    return (1/3)*x**3 + (1/2)*x**2 + x + 1
+
+
+def func3(x):
+    return 3/(1+x)
+
+
+def func4(x):
+    return 2*np.cos(x)
+
+
+def func5(x):
+    return np.sin(2 * x + 2)
+
+
+def func6(x):
+    return 2 * x ** 3 - 4 * x ** 2 + 8 * x - 3
+
+
+def func7(x):
+    return 2 ** x + 3
+
+
+def func8(x):
+    return func1(x) + func3(x)
