952ccdba771348d1dc32edd242eae9c76f7e73a2
Frank <reppepf@gmail.com>
2024-05-28 19:01:20 +0200

    safe approximation

--- zad5/approximation.py
+++ zad5/approximation.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.integrate import quad
 
 import integral as itg
 import laguerre as lg
@@ -17,9 +16,9 @@ def sum_weight_times_laguerre(coefficients):
     return result
 
 
-def shady_business(fun, acodin, benzo, high_array):
-    final_poly = sum_weight_times_laguerre(high_array)
-    x_lin = np.linspace(acodin, benzo, 100)
+def plot_error_coeffs(fun, a, b, coeffs):
+    final_poly = sum_weight_times_laguerre(coeffs)
+    x_lin = np.linspace(a, b, 100)
     poly_error = np.sqrt(np.mean(h.horner(x_lin, final_poly) - fun(x_lin)) ** 2)
     plt.plot(x_lin, h.horner(x_lin, final_poly))
     plt.plot(x_lin, fun(x_lin), color='red')
@@ -33,11 +32,10 @@ def iter_approximation(function, a, b, n):
     for k in range(n):
         laguerre_poly = np.array(lg.laguerre(k))
         integral = lambda x: function(x) * h.horner(x, laguerre_poly) * np.exp(-x)
-        # coeff = quad(integral, a, b)
         coeff = itg.integral(integral, a, b, 1e-5)
         laguerre_coeffs.append(laguerre_poly * coeff)
 
-    return n, shady_business(function, a, b, laguerre_coeffs)
+    return n, plot_error_coeffs(function, a, b, laguerre_coeffs)
 
 
 def err_approximation(function, a, b, error):
@@ -48,17 +46,16 @@ def err_approximation(function, a, b, error):
     while poly_error > error:
         laguerre_poly = np.array(lg.laguerre(k))
         integral = lambda x: function(x) * h.horner(x, laguerre_poly) * np.exp(-x)
-        coeff = quad(integral, a, b)    # faster
-        # coeff = inte.integral(integral, a, b, 1e-5)
-        laguerre_coeffs.append(laguerre_poly * coeff[0])
+        coeff = itg.integral(integral, a, b, 1e-5)
+        laguerre_coeffs.append(laguerre_poly * coeff)
         k += 1
-        if np.any(abs(laguerre_coeffs[-1]) < 1e-320) and np.any(abs(laguerre_coeffs[-1]) > 0):
+        if np.any(abs(laguerre_coeffs[-1]) < 1e-42) and np.any(abs(laguerre_coeffs[-1]) > 0):
             break
 
         final_poly = sum_weight_times_laguerre(laguerre_coeffs)
         poly_error = np.sqrt(np.mean(h.horner(x_lin, final_poly) - func(x_lin)) ** 2)
 
-    return k, shady_business(function, a, b, laguerre_coeffs)
+    return k, plot_error_coeffs(function, a, b, laguerre_coeffs)
 
 
 def format_output(data):
@@ -69,6 +66,6 @@ def func(x):
     return 2 * x ** 3 - 4 * x ** 2 + 8 * x - 3
 
 
-print(format_output(iter_approximation(func, 0, 1, 20)))
+print(format_output(iter_approximation(func, 1, 5, 20)))
 
-print(format_output(err_approximation(func, 0, 1, 0.04)))
+print(format_output(err_approximation(func, 1, 5, 0.1)))
